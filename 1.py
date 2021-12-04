e8d65624a005713180762ff994c47d1d77aff778
MaxOmenes <ukhin.maxim@gmail.com>
2021-12-04 20:21:25 +0300

    V 0.3

--- 1.py
+++ 1.py
@@ -1,27 +1,9 @@
 import codecs
 
-#modules
-def menu_point(mode):
-    '''
-        menu_point select text reading mode
-        returns the final text in variable
-        @MaxOmenes
-    '''
-    if mode == 'f':
-        file_path = str(input("Input file path: "))
-        f = codecs.open(file_path, "r", encoding='utf-8')
-        text = f.read()
-    elif mode == 'i':
-        text = str(input())
-    else:
-        exit()
-
-    return text
-
-
 #entry_point
-reading_mode = str(input("Input reading mode(f - file, i - input): "))
-text = menu_point(reading_mode)
+file_path = str(input("Input file path: "))
+f = codecs.open(file_path, "r", encoding='utf-8')
+text = f.read()
 #count_alghorithm
 word_book = {}
 word = ""
