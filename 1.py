590e77aaa318ac88af5e0134a3eb38ef14d20755
Max Omenes <46383741+MaxOmenes@users.noreply.github.com>
2021-12-05 00:32:43 +0300

    V 1.0

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
 import codecs
-from collections import OrderedDict
 
 #entry_point
 file_path = str(input("Input file path: "))
@@ -10,18 +9,17 @@ word_book = {}
 word = ""
 special_symbols = [".", ",","-","—", "?", "!", "\r", "\n", "(", ")", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":"]
 for letter in text:
-    if letter in special_symbols:
+    if letter in special_symbols: #change all ".", ",", "-", "?", "!" to "" @MaxOmenes
         letter = ""
-    if letter != " ": #change all ".", ",", "-", "?", "!" to "" @MaxOmenes
+    if letter != " ": 
         word+=letter
     else:
         if not (word in word_book):
             word_book[word] = 0 #add new word in list
         word_book[word]+=1
-        word = ""
+        word = "" #writing a new word
 
-#write dict sort @MaxOmenes/
-#write dict sort @MaxOmenes///
+#write dict sort @MaxOmenes
 word_book = {key: value for key, value in sorted(word_book.items(), key= lambda value: value[1], reverse=True)}
 
 k = 60
@@ -30,4 +28,4 @@ for element in word_book:
     k-=1
     if k < 0:
         break
-
+        
